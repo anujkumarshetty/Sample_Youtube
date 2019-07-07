b009c0a4636bb3773198967e0e091bc08091f9d4
Pramod <pramodmithyantha@gmail.com>
2019-07-07 12:44:45 +0530

    Search Bar

---
+++ src/Components/Search/Search.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Search-Style.css';
+import Constants from '../../Constants/Constants';
+
+function Search(props) {
+    return (
+        <div className="search">
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder={Constants.search_place_holder}
+                value={props.input}
+                onChange={props.inputChange}
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-success" 
+                    type="button" 
+                    onClick={props.search}
+                    >Search</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
---
+++ src/Constants/Constants.js
@@ -0,0 +1,6 @@
+export default {
+    max_result : 6,
+    search_place_holder : "Search here...",
+    embedUrl : "https://www.youtube.com/embed/",
+    errorAlert : "Please try again... Error : " 
+}   
