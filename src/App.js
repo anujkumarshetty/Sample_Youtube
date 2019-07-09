2391ac4174383c6b59ee4621dcff1a774c84edc4
Pramod M <pramodmithyantha@gmail.com>
2019-07-09 08:29:58 +0530

    Youtube Search bar (#3)
    * Search Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import Search from './Components/Search/Search';
+import image from './Images/youtube.png';
+import ApiCalls from './Components/API/apiCalls';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      URL: []
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState(
+      {
+        input: e.target.value
+      }
+    )
+  }
+
+  handleSearch =  () => {
+    ApiCalls.searchApi(this.state.input).then((resp)=>{
+      this.setState({ URL: resp })
+    })
+  }
 
   render() {
     return (
       <div className="App">
-        <h3>Hello World</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-6 col-md-3">
+              <img src={image} width="134px" height="79px" alt="Logo" />
+            </div>
+            <div className="col-12 col-md-7">
+              <Search input={this.state.input}
+                inputChange={this.handleInputChange}
+                search={this.handleSearch} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
